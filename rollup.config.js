0cf64c64d062e527c372f74cebfd5ab7098f89c1
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-11-09 05:17:32 +0530

    fix: removed to reduce package size
    This doesnt help at all because you need to get it from a cdn or public folder in the next repo so should not increase package size for this seperately

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,6 @@ import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json' assert { type: 'json' };
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import copy from 'rollup-plugin-copy';
 
 export default [
     // Browser build
@@ -23,14 +22,6 @@ export default [
             }),
             nodeResolve(),
             commonjs({ browser: true }),
-            copy({
-                targets: [
-                    {
-                        src: 'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
-                        dest: 'dist',
-                    },
-                ],
-            }),
         ],
     },
 ];
